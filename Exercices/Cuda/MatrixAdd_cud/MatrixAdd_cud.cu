b30bf906011971d0c22056ccb579a02897ab7aa0
EUROGICIEL\franck.ginguene <franck.ginguene@scalian.com>
2020-12-08 00:09:00 +0100

    refonte matrix add 1D

--- Exercices/Cuda/MatrixAdd_cud/MatrixAdd_cud.cu
+++ Exercices/Cuda/MatrixAdd_cud/MatrixAdd_cud.cu
@@ -1,97 +1,86 @@
 #include <stdio.h>
 
-#define NB_COLS 8192  // Nombre de colonnes de la matrice.
-#define NB_ROWS 8192  // Nombre de lignes de la matrice.
-#define NB_THR_X 32   // Nombre de threads par bloc en X.
-#define NB_THR_Y 8   // Nombre de threads par bloc en Y.
-#define ITER_PER_THR 4
+#define NB_COLS 4000  // Nombre de colonnes de la matrice.
+#define NB_ROWS	4000  // Nombre de lignes de la matrice.
 
-void matrixInit (int *mat);   // Initialisation d'une matrice.
-void checkRes (int *mat);     // Vérification des résultats.
-
-#define DIVUP(x,y) (((x)+(y)-1)/(y))
-#define IMUL(a,b) __mul24(a,b)
+void matrixInit(int *mat);   // Initialisation d'une matrice.
+void checkRes(int *mat);     // Vérification des résultats.
 
 // Noyau CUDA
-
-__global__ void MatrixAdd (int *a, int *b, int *c)
+__global__ void MatrixAdd(int *a, int *b, int *c)
 {
-    int thrX = blockIdx.x * blockDim.x + threadIdx.x;
-    int thrY = blockIdx.y * blockDim.y * ITER_PER_THR + threadIdx.y;
-    int index = thrY * NB_COLS + thrX;
-
-    if ((thrX < NB_COLS) && (thrY < NB_ROWS)) {
-        c[index] = a[index] + b[index];
-        index += NB_COLS * NB_THR_Y;
-        c[index] = a[index] + b[index];
-        index += NB_COLS * NB_THR_Y;
-        c[index] = a[index] + b[index];
-        index += NB_COLS * NB_THR_Y;
-        c[index] = a[index] + b[index];
-    }
+	// TODO
+	// indices : calculez l'indice global à l'aide de 
+	// blockIdx.x
+	// blockDim.x
+	// threadIdx.x
 }
 
 // Code du Host
-
-int main (void)
+int main(void)
 {
-    int *a, *b, *c;              // Matrices A, B et C du host
-    int *dev_a, *dev_b, *dev_c;  // Matrices A, B et C du device
+	int *a, *b, *c;              // Matrices A, B et C du host
+	int *dev_a, *dev_b, *dev_c;  // Matrices A, B et C du device
 
-    dim3 threadsPerBlock (NB_THR_X, NB_THR_Y);                                 // Nombre de threads par blocs (en X et en Y).
-    dim3 blocksPerGrid (DIVUP(NB_COLS, NB_THR_X), DIVUP(NB_ROWS, (NB_THR_Y * ITER_PER_THR)));   // Nombre de blocs, en X et en Y.
+	int nbElements = NB_COLS * NB_ROWS;
+	int matrixSize = nbElements * sizeof(int);
+	int threadsPerBlock = 256;
+	// TODO calculez le nombre de blocs nécessaires
+	int blocksPerGrid = 0;
 
-    int matrixSizeB = NB_ROWS * NB_COLS * sizeof(int);
+	// Allocation des matrices du host.
+	a = (int *)malloc(matrixSize);
+	if (a == NULL) { printf("Allocation failure\n"); abort(); }
 
-    // Allocation des matrices du host.
+	b = (int *)malloc(matrixSize);
+	if (b == NULL) { printf("Allocation failure\n"); abort(); }
 
-    a = (int *)malloc(matrixSizeB);
-    if (a == NULL) { printf ("Allocation failure\n"); abort();}
+	c = (int *)malloc(matrixSize);
+	if (c == NULL) { printf("Allocation failure\n"); abort(); }
 
-    b = (int *)malloc(matrixSizeB);
-    if (b == NULL) { printf ("Allocation failure\n"); abort();}
+	// Allocation des matrices du device.
+	cudaMalloc((void **)&dev_a, matrixSize);
+	cudaMalloc((void **)&dev_b, matrixSize);
+	cudaMalloc((void **)&dev_c, matrixSize);
+	auto cudaStatus = cudaGetLastError();
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "addKernel memory failed: %s\n", cudaGetErrorString(cudaStatus));
+		return -1;
+	}
 
-    c = (int *)malloc(matrixSizeB);
-    if (c == NULL) { printf ("Allocation failure\n"); abort();}
+	// Initialisation des matrices A et B.
+	matrixInit(a);
+	matrixInit(b);
 
-    // Allocation des matrices du device.
+	// Copie des matrices A et B sur le GPU.
+	// TODO transférer les données a et b vers le GPU
+	// utilisez cudaMemcpy et cudaMemcpyHostToDevice
 
-    cudaMalloc ( (void **) &dev_a, matrixSizeB);
-    cudaMalloc ( (void **) &dev_b, matrixSizeB);
-    cudaMalloc ( (void **) &dev_c, matrixSizeB);
+	// Lancement du noyau.
+	// TODO lancer le kernel avec le nombre de bloc et le nombre de threads par bloc adéquats
+	cudaStatus = cudaGetLastError();
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+		return -1;
+	}
 
-    // Initialisation des matrices A et B.
+	// Copie de la matrice C du GPU vers le host.
+	cudaMemcpy(c, dev_c, matrixSize, cudaMemcpyDeviceToHost);
 
-    matrixInit(a);
-    matrixInit(b);
+	checkRes(c);
 
-    // Copie des matrices A et B sur le GPU.
+	// Libération des matrices host et device.
+	free(a);
+	free(b);
+	free(c);
 
-    cudaMemcpy ( dev_a, a, matrixSizeB, cudaMemcpyHostToDevice) ;
-    cudaMemcpy ( dev_b, b, matrixSizeB, cudaMemcpyHostToDevice) ;
-
-    // Lancement du noyau.
-
-    MatrixAdd<<<blocksPerGrid, threadsPerBlock>>>( dev_a, dev_b, dev_c );
-
-    // Copie de la matrice C du GPU vers le host.
-    cudaMemcpy ( c, dev_c, matrixSizeB, cudaMemcpyDeviceToHost) ;
-
-    checkRes (c);
-
-    // Libération des matrices host et device.
-
-    free (a);
-    free (b);
-    free (c);
-
-    cudaFree ( dev_a ) ;
-    cudaFree ( dev_b ) ;
-    cudaFree ( dev_c ) ;
+	cudaFree(dev_a);
+	cudaFree(dev_b);
+	cudaFree(dev_c);
 
 	getchar();
 
-    return 0 ;
+	return 0;
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////
@@ -102,26 +91,26 @@ int main (void)
 
 void matrixInit(int *mat)
 {
-    int l, c;
+	int l, c;
 
-    for (l = 0; l < NB_ROWS; l++)
-        for (c = 0; c < NB_COLS; c++)
-            mat[l * NB_COLS + c] = l + c;
+	for (l = 0; l < NB_ROWS; l++)
+		for (c = 0; c < NB_COLS; c++)
+			mat[l * NB_COLS + c] = l + c;
 }
 
 void checkRes(int *mat)
 {
-    int l, c;
+	int l, c;
 
-    for (l = 0; l < NB_ROWS; l++)
-        for (c = 0; c < NB_COLS; c++)
-            if (mat[l * NB_COLS + c] != 2 * (c + l)) {
-                printf ("Erreur de calcul sur l'élément %d:%d :\n", l, c);
-                printf (" - Valeur calculée : %d\n", mat[l * NB_COLS + c]);
-                printf (" - Valeur attendue : %d\n", 2 * (c + l));
-                exit(0);
-            }
+	for (l = 0; l < NB_ROWS; l++)
+		for (c = 0; c < NB_COLS; c++)
+			if (mat[l * NB_COLS + c] != 2 * (c + l)) {
+				printf("Erreur de calcul sur l'element %d:%d :\n", l, c);
+				printf(" - Valeur calculee : %d\n", mat[l * NB_COLS + c]);
+				printf(" - Valeur attendue : %d\n", 2 * (c + l));
+				exit(0);
+			}
 
-    printf ("NIVEAU 1: Terminé\n");
-    printf ("Bravo !\n");
+	printf("LEVEL 1: Done\n");
+	printf("Good job!\n");
 }
